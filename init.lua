4b71b87123c2904bad967798cef1935ba27b1575
Kulwinderpal <113812306+Kulwinderpal@users.noreply.github.com>
2024-11-21 19:07:40 -0800

    Updated init.lua

--- init.lua
+++ init.lua
@@ -1,23 +1,26 @@
+-- Yank Items to the System Clipboard by Default
+vim.opt.clipboard = "unnamedplus"
+
 -- Improve Startup Time
 vim.loader.enable()
 
 -- Add Line Numbers
-vim.cmd "set number"
+vim.cmd("set number")
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-        local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-        local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-        if vim.v.shell_error ~= 0 then
-                vim.api.nvim_echo({
-                        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-                        { out, "WarningMsg" },
-                        { "\nPress any key to exit..." },
-                }, true, {})
-                vim.fn.getchar()
-                os.exit(1)
-        end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out,                            "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
